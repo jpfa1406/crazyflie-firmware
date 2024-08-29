d10179dd7e0c91c20e21595ea7fd06ed8ddd6994
joao <joao>
2024-08-29 09:31:37 -0300

    aula 5

--- programs/chapters/ch04.h
+++ programs/chapters/ch04.h
@@ -1,7 +1,10 @@
 #include "mbed.h"
 #include "crazyflie.h"
 
-PwmOut motor3(MOTOR1);
+PwmOut motor1(MOTOR1);
+PwmOut motor2(MOTOR2);
+PwmOut motor3(MOTOR3);
+PwmOut motor4(MOTOR4);
 
 float control_motor(float omega){
     float PWM = a2*omega*omega + a1*omega; 
@@ -10,10 +13,22 @@ float control_motor(float omega){
 
 int main()
 {
+    float speed = 2000.0;
+
+    motor1.period(1.0/500.0);
+    motor1 = control_motor(speed);
+    motor2.period(1.0/500.0);
+    motor2 = control_motor(speed);
     motor3.period(1.0/500.0);
-    motor3 = control_motor(1000.0);
-    wait(5);
+    motor3 = control_motor(speed);
+    motor4.period(1.0/500.0);
+    motor4 = control_motor(speed);
+    wait(10);
+    motor1 = 0;
+    motor2 = 0;
     motor3 = 0;
+    motor4 = 0;
+
     while(true)
     { 
     }
